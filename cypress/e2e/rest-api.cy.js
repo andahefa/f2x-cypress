95a5193d4a533ec3377d423fc372aae42f2582f7
Anderson Hernandez <andahefa1995@gmail.com>
2024-03-09 21:41:53 -0500

    creation test consult and validation information users

--- cypress/e2e/rest-api.cy.js
+++ cypress/e2e/rest-api.cy.js
@@ -1,5 +1,28 @@
-describe('Validation API', () => {
-  it('Consult information', () => {
 
+describe('Validation API', () => {
+  const baseUrl = 'https://reqres.in/api'
+  const endpoint = '/users'
+  it('Consult and validation information users', () => {
+    const param = '?page=1'
+    cy.request('GET', baseUrl + endpoint + param).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body).to.have.property('page')
+      expect(response.body).to.have.property('data')
+
+      const data = response.body.data;
+      for (const user of data) {
+        const id = JSON.stringify(user.id)
+        const email = JSON.stringify(user.email)
+        const firstName = JSON.stringify(user.first_name)
+        const lastName = JSON.stringify(user.last_name)
+        const avatar = JSON.stringify(user.avatar)
+
+        expect(id).to.not.be.null
+        expect(email).to.not.be.null
+        expect(firstName).to.not.be.null
+        expect(lastName).to.not.be.null
+        expect(avatar).to.not.be.null
+      }
+    });
   })
 })
